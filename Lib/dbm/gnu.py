797e13de223ead7c513baf1d559bd29ae73def5d
Felix Krull <f_krull@gmx.de>
2014-03-10 17:25:22 +0100

    Fixed tkinter and gdbm import messages to refer to correct packages.
    I.e. PVER-tk and PVER-gdbm instead of python3-tk and python3-gdbm.
    
    --HG--
    branch : precise

--- Lib/dbm/gnu.py
+++ Lib/dbm/gnu.py
@@ -1,6 +1,8 @@
 """Provide the _gdbm module as a dbm submodule."""
 
+import sys
+
 try:
     from _gdbm import *
 except ImportError as msg:
-    raise ImportError(str(msg) + ', please install the python3-gdbm package')
+    raise ImportError(str(msg) + ', please install the python%s.%s-gdbm package' % sys.version_info[:2])

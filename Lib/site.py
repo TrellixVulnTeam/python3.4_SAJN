f5e528a0597622bd228244169a50bf04bf78cd59
Felix Krull <f_krull@gmx.de>
2015-01-28 16:42:34 +0100

    Fix virtualenv module installation.
    --HG--
    branch : precise

--- Lib/site.py
+++ Lib/site.py
@@ -317,7 +317,7 @@ def getsitepackages(prefixes=None):
         seen.add(prefix)
 
         if os.sep == '/':
-            if 'VIRTUAL_ENV' in os.environ or sys.base_prefix != sys.prefix:
+            if 'VIRTUAL_ENV' in os.environ or 'real_prefix' in sys.__dict__ or sys.base_prefix != sys.prefix:
                 sitepackages.append(os.path.join(prefix, "lib",
                                                  "python" + sys.version[:3],
                                                  "site-packages"))
